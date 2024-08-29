87bd4f343aabfa6af84cd7ac3a2208922d481b50
VadimDzemidzenka <demidenko_92@tut.by>
2024-08-29 18:11:27 +0300

    avgust

--- lesson7/shop/Test/test_3.py
+++ lesson7/shop/Test/test_3.py
@@ -1,19 +1,20 @@
-from Lesson_7.shop.Pages.shopcontainer import ShopContainer
-from Lesson_7.shop.Pages.shopmain import ShopmainPage
+from lesson7.shop.Pages.shopcontainer import ShopContainer
+from lesson7.shop.Pages.shopmain import ShopmainPage
+
 
 def test_shop(chrome_browser):
     expected_total = "58.29"
-    
+
     shopmain = ShopmainPage(chrome_browser)
     shopmain.registration_fields()
     shopmain.by_issue()
     shopmain.click_issue()
     shopmain.into_container()
-    
+
     container = ShopContainer(chrome_browser)
     container.checkout()
     container.info()
     container.price()
-    
+
     assert expected_total in container.price()
     print(f"Итоговая сумма равна ${container.price()}")
