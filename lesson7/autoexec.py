5d6eaaa9c4da56cdfd67ba473e0fac14c10b87fc
VadimDzemidzenka <demidenko_92@tut.by>
2024-08-29 17:56:30 +0300

    Delete lesson7 directory

--- lesson7/autoexec.py
+++
@@ -1,3 +0,0 @@
-data_type_URL = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
-calculator_URL = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
-shop_URL = "https://www.saucedemo.com/"
--- lesson7/calculator/Test/test2.py
+++
@@ -1,7 +0,0 @@
-from Lesson_7.calculator.Pages.Calcmainpage import CalcMain
-
-def test_calculator_assert(chrome_browser):
-    calsmain = CalcMain(chrome_browser)
-    calsmain.insert_time()
-    calsmain.clicking_buttons()
-    assert "15" in calsmain.wait_button_gettext()
